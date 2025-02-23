19375e8a811c6fe04393a00759655a375ef7612e
Hosam Mohammed Ahmed Yousif <118902100+hosam21@users.noreply.github.com>
2025-02-23 19:19:37 +0200

    QAFILE_RAQI_LMS_LASTVERSION.py

--- QAFILE_RAQI_LMS_LASTVERSION.py
+++ QAFILE_RAQI_LMS_LASTVERSION.py
@@ -155,11 +155,12 @@ def process_pdf(pdf_path):
     
     # --- Build vectorstore and document store ---
     embedding_function = OpenSourceEmbeddings()
+  
+    persist_directory = os.path.join(os.getcwd(), "chroma_db")
     client_settings = Settings(
-    chroma_db_impl="duckdb+parquet",
-    persist_directory="chroma_db"
+    persist_directory=persist_directory,
+    is_persistent=True
     )
-
     vectorstore = Chroma(
     collection_name="multi_modal_rag",
     embedding_function=embedding_function,
